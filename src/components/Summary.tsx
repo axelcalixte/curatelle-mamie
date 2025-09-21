32c10c75a3978aacfae18ac86e295d40fc45e6d3
Axel Calixte <axelcalixte@mailbox.org>
2025-09-21 17:57:52 +0200

    export tier categorizing preferences!

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,65 +1,93 @@
-import { For } from "solid-js";
+import { For, onMount } from "solid-js";
 import { store } from "../state";
 
 export default function Summary() {
-  const rows = () => store.rows;
-  const subCatsSums = new Map<string, number>();
+   const rows = () => store.rows;
+   const subCatsSums = new Map<string, number>();
 
-  function getEveryUniqueSubCats() {
-    for (const row of rows()) {
-      if (!subCatsSums.has(row.subCategory!)) {
-        subCatsSums.set(row.subCategory!, row.value * 100);
-      } else {
-        subCatsSums.set(
-          row.subCategory!,
-          row.value * 100 + subCatsSums.get(row.subCategory!)!,
-        );
+   let exportButton!: HTMLButtonElement;
+
+   function getEveryUniqueSubCats() {
+      for (const row of rows()) {
+         if (!subCatsSums.has(row.subCategory!)) {
+            subCatsSums.set(row.subCategory!, row.value * 100);
+         } else {
+            subCatsSums.set(
+               row.subCategory!,
+               row.value * 100 + subCatsSums.get(row.subCategory!)!,
+            );
+         }
       }
-    }
-  }
+   }
 
-  function mappedSums() {
-    function cached() {
-      if (subCatsSums.size > 0) return subCatsSums;
-      else {
-        getEveryUniqueSubCats();
-        return subCatsSums;
+   function mappedSums() {
+      function cached() {
+         if (subCatsSums.size > 0) return subCatsSums;
+         else {
+            getEveryUniqueSubCats();
+            return subCatsSums;
+         }
       }
-    }
-    return [...cached()].map(([key, value]) => ({ key, value }));
-  }
+      return [...cached()].map(([key, value]) => ({ key, value }));
+   }
 
-  return (
-    <>
-      <h3 class="title is-3"> RESSOURCES </h3>
+   function createMapOfLibelleCats() {
+      return JSON.stringify(
+         store.tiers
+            .filter(tier => tier.mainCategory && tier.subCategory)
+            .map(tier => [tier.label, [tier.mainCategory, tier.subCategory]])
+      );
+   }
 
-      <table class="table">
-        <tbody>
-          <For each={mappedSums().filter((x) => x.value > 0)}>
-            {(entry) => (
-              <tr>
-                <td>{entry.key}</td>
-                <td>{entry.value / 100}</td>
-              </tr>
-            )}
-          </For>
-        </tbody>
-      </table>
+   onMount(() => {
+      exportButton.addEventListener("click", async () => {
+         const handle = await window.showSaveFilePicker({
+            startIn: "documents",
+            suggestedName: "comptes_de_mamie_sauvegarde.json",
+            types: [{
+               description: "JaveScript Object Notation file",
+               accept: { "application/json": [".json"] },
+            }]
+         });
+         const writer = await handle.createWritable();
+         await writer.write(createMapOfLibelleCats());
+         await writer.close();
+      })
+   })
 
-      <h3 class="title is-3"> DEPENSES </h3>
+   return (
+      <>
+         <h3 class="title is-3"> RESSOURCES </h3>
 
-      <table class="table">
-        <tbody>
-          <For each={mappedSums().filter((x) => x.value < 0)}>
-            {(entry) => (
-              <tr>
-                <td>{entry.key}</td>
-                <td>{entry.value / 100}</td>
-              </tr>
-            )}
-          </For>
-        </tbody>
-      </table>
-    </>
-  );
+         <table class="table">
+            <tbody>
+               <For each={mappedSums().filter((x) => x.value > 0)}>
+                  {(entry) => (
+                     <tr>
+                        <td>{entry.key}</td>
+                        <td>{entry.value / 100}</td>
+                     </tr>
+                  )}
+               </For>
+            </tbody>
+         </table>
+
+         <h3 class="title is-3"> DEPENSES </h3>
+
+         <table class="table">
+            <tbody>
+               <For each={mappedSums().filter((x) => x.value < 0)}>
+                  {(entry) => (
+                     <tr>
+                        <td>{entry.key}</td>
+                        <td>{entry.value / 100}</td>
+                     </tr>
+                  )}
+               </For>
+            </tbody>
+         </table>
+
+         <button class="button is-info" ref={exportButton}> Exporter la sauvegarde </button>
+      </>
+   );
 }
